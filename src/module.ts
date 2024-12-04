04fd0c80423f2fe3c16f15625afdfc836ce369ab
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-12-04 23:30:41 +0200

    Remove runtime plugin

--- src/module.ts
+++ src/module.ts
@@ -11,9 +11,5 @@ export default defineNuxtModule<ModuleOptions>({
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
-
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
   },
 })
